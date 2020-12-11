454806c312a534782e46340bd5d1e9c1c5fe70ce
Ndn1618 <ndilbar1618@gmail.com>
2020-12-11 09:38:47 +0500

    Add data js file

---
+++ js/data.js
@@ -0,0 +1,114 @@
+const russianLetters = [
+  {
+    id: 119,
+    value: 'в',
+  },
+  {
+    id: 101,
+    value: 'е',
+  },
+  {
+    id: 114,
+    value: 'р',
+  },
+  {
+    id: 116,
+    value: 'т',
+  },
+  {
+    id: 121,
+    value: 'й',
+  },
+  {
+    id: 117,
+    value: 'у',
+  },
+  {
+    id: 105,
+    value: 'и',
+  },
+  {
+    id: 111,
+    value: 'о',
+  },
+  {
+    id: 112,
+    value: 'п',
+  },
+  {
+    id: 97,
+    value: 'а',
+  },
+  {
+    id: 115,
+    value: 'с',
+  },
+  {
+    id: 100,
+    value: 'д',
+  },
+  {
+    id: 102,
+    value: 'ф',
+  },
+  {
+    id: 103,
+    value: 'г',
+  },
+  {
+    id: 104,
+    value: 'х',
+  },
+  {
+    id: 106,
+    value: 'ж',
+  },
+  {
+    id: 107,
+    value: 'к',
+  },
+  {
+    id: 108,
+    value: 'л',
+  },
+  {
+    id: 122,
+    value: 'з',
+  },
+  {
+    id: 120,
+    value: 'х',
+  },
+  {
+    id: 99,
+    value: 'с',
+  },
+  {
+    id: 118,
+    value: 'в',
+  },
+  {
+    id: 98,
+    value: 'б',
+  },
+  {
+    id: 110,
+    value: 'н',
+  },
+  {
+    id: 109,
+    value: 'м',
+  },
+  {
+    id: 39,
+    value: 'ъ',
+  },
+  {
+    id: 32,
+    value: ' ',
+  },
+  {
+    id: 113,
+    value: 'қ',
+  },
+];
