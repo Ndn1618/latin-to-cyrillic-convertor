d2846fbf5a19a1b537f9a1385eaa8b57badd4025
Ndn1618 <ndilbar1618@gmail.com>
2020-12-11 21:13:59 +0500

    Add cryillic capilat letters data

--- js/data.js
+++ js/data.js
@@ -4,104 +4,224 @@ export const russianLetters = [
     value: 'в',
   },
   {
+    id: 87,
+    value: 'В',
+  },
+  {
     id: 101,
     value: 'е',
+  },
+  {
+    id: 69,
+    value: 'Е',
   },
   {
     id: 114,
     value: 'р',
   },
   {
+    id: 82,
+    value: 'Р',
+  },
+  {
     id: 116,
     value: 'т',
+  },
+  {
+    id: 84,
+    value: 'Т',
   },
   {
     id: 121,
     value: 'й',
   },
   {
+    id: 89,
+    value: 'Й',
+  },
+  {
     id: 117,
     value: 'у',
+  },
+  {
+    id: 85,
+    value: 'У',
   },
   {
     id: 105,
     value: 'и',
   },
   {
+    id: 73,
+    value: 'И',
+  },
+  {
     id: 111,
     value: 'о',
+  },
+  {
+    id: 79,
+    value: 'О',
   },
   {
     id: 112,
     value: 'п',
   },
   {
+    id: 80,
+    value: 'П',
+  },
+  {
     id: 97,
     value: 'а',
+  },
+  {
+    id: 65,
+    value: 'А',
   },
   {
     id: 115,
     value: 'с',
   },
   {
+    id: 83,
+    value: 'С',
+  },
+  {
     id: 100,
     value: 'д',
+  },
+  {
+    id: 68,
+    value: 'Д',
   },
   {
     id: 102,
     value: 'ф',
   },
   {
+    id: 70,
+    value: 'Ф',
+  },
+  {
     id: 103,
     value: 'г',
   },
   {
+    id: 71,
+    value: 'Г',
+  },
+  {
     id: 104,
-    value: 'х',
+    value: 'ҳ',
+  },
+  {
+    id: 72,
+    value: 'Ҳ',
   },
   {
     id: 106,
     value: 'ж',
   },
   {
+    id: 74,
+    value: 'Ж',
+  },
+  {
     id: 107,
     value: 'к',
+  },
+  {
+    id: 75,
+    value: 'К',
   },
   {
     id: 108,
     value: 'л',
   },
   {
+    id: 76,
+    value: 'Л',
+  },
+  {
     id: 122,
     value: 'з',
+  },
+  {
+    id: 90,
+    value: 'З',
   },
   {
     id: 120,
     value: 'х',
   },
   {
+    id: 88,
+    value: 'Х',
+  },
+  {
     id: 99,
     value: 'с',
+  },
+  {
+    id: 67,
+    value: 'С',
   },
   {
     id: 118,
     value: 'в',
   },
   {
+    id: 86,
+    value: 'В',
+  },
+  {
     id: 98,
     value: 'б',
+  },
+  {
+    id: 66,
+    value: 'Б',
   },
   {
     id: 110,
     value: 'н',
   },
   {
+    id: 78,
+    value: 'Н',
+  },
+  {
     id: 109,
     value: 'м',
   },
   {
+    id: 77,
+    value: 'М',
+  },
+  {
     id: 39,
     value: 'ъ',
+  },
+  {
+    id: 34,
+    value: `"`,
+  },
+  {
+    id: 33,
+    value: '!',
+  },
+  {
+    id: 63,
+    value: '?',
+  },
+  {
+    id: 46,
+    value: '.',
+  },
+  {
+    id: 44,
+    value: ',',
   },
   {
     id: 32,
@@ -110,5 +230,9 @@ export const russianLetters = [
   {
     id: 113,
     value: 'қ',
+  },
+  {
+    id: 81,
+    value: 'Қ',
   },
 ];
